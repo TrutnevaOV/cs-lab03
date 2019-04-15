7ce84418b2c5ae17bd8528294280ebabdda8a7fd
TrutnevaOV <trutneva.o@list.ru>
2019-04-15 03:03:39 +0300

    code: масштабирование

--- main.cpp
+++ main.cpp
@@ -113,18 +113,32 @@ void svg_rect(double x, double y, double width, double height, string stroke = "
 void
 show_histogram_svg(const vector<size_t>& bins)
 {
-
     const auto IMAGE_WIDTH = 400;
     const auto IMAGE_HEIGHT = 300;
     const auto TEXT_LEFT = 20;
     const auto TEXT_BASELINE = 20;
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
+    const size_t MAX_WIDTH = IMAGE_WIDTH - TEXT_LEFT - TEXT_WIDTH;
+    size_t max_count = 0;
+    for (size_t count : bins)
+    {
+        if (count > max_count)
+        {
+            max_count = count;
+        }
+    }
+    size_t scaling_factor = 10;
+    const bool scaling_needed = (max_count*scaling_factor) > MAX_WIDTH;
+    if (scaling_needed)
+    {
+        scaling_factor = (double)MAX_WIDTH / max_count;
+    }
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     double top = 0;
     for (size_t bin : bins)
     {
-        const double bin_width = 10 * bin;
+        size_t bin_width = (size_t)(bin * scaling_factor);
         svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
         svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "blue", "pink");
         top += BIN_HEIGHT;
