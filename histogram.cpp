66f251882b57d0d8f439a816e8b3148dd85e8cea
TrutnevaOV <trutneva.o@list.ru>
2019-04-16 04:16:11 +0300

    code: коммит всех файлов для модульного тестирования

---
+++ histogram.cpp
@@ -0,0 +1,26 @@
+#include "histogram.h"
+#include <vector>
+
+using namespace std;
+
+void
+find_minmax(const vector<double>& numbers, double& min, double& max)
+{
+    if (numbers.size()==0);
+    else
+    {
+        min = numbers[0];
+        max = numbers[0];
+        for (double number : numbers)
+        {
+            if (number < min)
+            {
+                min = number;
+            }
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+    }
+}
