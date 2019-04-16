604239362d344ba47301649fd8176c220b6ea980
TrutnevaOV <trutneva.o@list.ru>
2019-04-16 04:33:23 +0300

    code: выделение всех фенкций svg в отдельный модуль

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "histogram.h"
+#include "svg.h"
 using namespace std;
 
 vector<double>
@@ -69,67 +70,6 @@ show_histogram_text( vector<size_t> bins)
         cout << '\n';
     }
 
-}
-
-void
-svg_begin(double width, double height)
-{
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg width='" << width << "' height='" << height << "' "
-         << "viewBox='0 0 " << width << " " << height << "' "
-         << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
-
-void
-svg_end()
-{
-    cout << "</svg>\n";
-}
-
-void svg_text(double left, double baseline, string text)
-{
-    cout<<"<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";
-}
-
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
-{
-    cout<< "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
-}
-
-void
-show_histogram_svg(const vector<size_t>& bins)
-{
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const size_t MAX_WIDTH = IMAGE_WIDTH - TEXT_LEFT - TEXT_WIDTH;
-    size_t max_count = 0;
-    for (size_t count : bins)
-    {
-        if (count > max_count)
-        {
-            max_count = count;
-        }
-    }
-    size_t scaling_factor = 10;
-    const bool scaling_needed = (max_count*scaling_factor) > MAX_WIDTH;
-    if (scaling_needed)
-    {
-        scaling_factor = (double)MAX_WIDTH / max_count;
-    }
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    double top = 0;
-    for (size_t bin : bins)
-    {
-        size_t bin_width = (size_t)(bin * scaling_factor);
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "blue", "pink");
-        top += BIN_HEIGHT;
-    }
-    svg_end();
 }
 
 int
